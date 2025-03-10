6b1f10330ef27e601b92a2dcad8bc3b63e31a60e
Pureheart Gharoro <gharoropureheart@gmail.com>
2025-03-10 16:47:31 +0100

    clear cache on load

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
+queryClient.clear();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
